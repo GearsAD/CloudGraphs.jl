376426c9a873f5dd78b1df65dffef294d5094e8c
Sam Claassens <sclaassens.ad@gmail.com>
2016-07-10 01:32:17 -0500

    Updated with empty credentials.

--- test/CloudGraphs.jl
+++ test/CloudGraphs.jl
@@ -7,7 +7,7 @@ using CloudGraphs;
 
 # Creating a connection
 print("[TEST] Connecting to the local CloudGraphs instance (Neo4j and Mongo)...");
-configuration = CloudGraphs.CloudGraphConfiguration("localhost", 7474, "neo4j", "neo5j", "localhost", 27017, false, "", "");
+configuration = CloudGraphs.CloudGraphConfiguration("localhost", 7474, "", "", "localhost", 27017, false, "", "");
 cloudGraph = connect(configuration);
 println("Success!");
 
