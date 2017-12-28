dc0a62380451f77f7f40cf8b5c6dcb9387fe5b5b
GearsAD <sclaassens.ad@gmail.com>
2017-12-28 00:35:18 -0600

    Refactored BigData methods and completed unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,11 @@ using Base.Test
 include("QuickPackProtoTest.jl")
 
 include("CloudGraphs.jl")
+
+# Big data tests
+if !haskey(ENV, "TRAVIS_OS_NAME")
+    include("BigData.jl")
+else
+  print("[TEST] NOTE: Testing in Travis, skipping the Mongo bigData test for the moment...")
+end
+    
