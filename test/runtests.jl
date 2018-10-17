afdfa881c690a6c163036a9081073996d3b61f9a
dehann <dehann.fourie@gmail.com>
2018-10-17 00:18:18 -0400

    adding binary tests back in

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,12 +10,12 @@ include("CloudGraphs.jl")
 
 
 # Big data tests
-# TODO: Get BigData tests to run again.
-if !haskey(ENV, "TRAVIS_OS_NAME")
-    include("BigData.jl")
-else
-  print("[TEST] NOTE: Testing in Travis, skipping the Mongo bigData test for the moment...")
-end
+# Maybe done: Get BigData tests to run again.
+# if !haskey(ENV, "TRAVIS_OS_NAME")
+include("BigData.jl")
+# else
+#   print("[TEST] NOTE: Testing in Travis, skipping the Mongo bigData test for the moment...")
+# end
 
 # Return the true exit status from FactCheck
 # FactCheck.exitstatus()
